ed8d60d4aada8cec654d21772923274212288ac6
Kimhyerim <ax1021@naver.com>
2025-03-20 09:07:49 +0900

    [Add] ScoreDTO 파일 추가

---
+++ HangMan/src/Model/ScoreDTO.java
@@ -0,0 +1,20 @@
+package Model;
+
+public class ScoreDTO {
+	private String score_id;
+	private String user_id;
+	private int f_score;
+	
+	
+	public String getScore_id() {
+		return score_id;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public int getF_score() {
+		return f_score;
+	}
+	
+	
+}
